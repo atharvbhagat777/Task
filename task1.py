a26edcf6fc4cb118071fd30421c4fe99e4cca9d2
Atharv bhagat <atharvbhagat777@gmail.com>
2026-08-08 23:56:21 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+# 1.Create a variable named pi and store the value 22/7 in it. Now check the data type of this variable.
+pi= 22/7
+print(type(pi))
+
+# 2. Create a variable called for and assign it a value 4. See what happens and find the reason behind the behavior that you see. 
+#for=4
+# for cannot be used as a variable 
+# for=4 show syntaxerror
+
+# 3. Store the principal amount, rate of interest, and time in different variables and then calculate the Simple Interest for 3 years. Formula: Simple Interest = P x R x T / 100 
+P=10000
+R=5
+T=3
+SI= P * R * T/100
+print(SI)
+#simple Interest for 3 year is 1500.0
